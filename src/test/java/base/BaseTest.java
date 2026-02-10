75846fc1dbb36cc48fc3d37ad71c1a8b9779da38
harikavanum5 <111125660+harikavanum5@users.noreply.github.com>
2026-02-10 15:57:47 +0530

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,96 +1,84 @@
-//package base;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.ITestResult;
-//import org.testng.annotations.AfterMethod;
-//import org.testng.annotations.BeforeMethod;
-//import org.testng.annotations.Listeners;
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import listeners.ExtentTestListener;
-//import utils.AllureUtils;
-//
-//
-//@Listeners(ExtentTestListener.class)
-//
-//public class BaseTest {
-//
-//    public WebDriver driver;
-//
-//    @BeforeMethod
-//    public void setup() {
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-//
-//    }
-//
-//    @AfterMethod
-//    public void tearDown() {
-//        if(driver != null) {
-//            driver.quit();
-//        }
-//    }
-// 
-//}
 package base;
 
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
+
+import com.aventstack.extentreports.ExtentReports;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import utils.AllureUtils;
+import utils.ExtentReportManager;
 
+@Listeners(listeners.ExtentTestListener.class)
 public class BaseTest {
 
+    protected static ExtentReports extent;
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
-    /* =================================
-       OPEN ONLY ONCE
-       ================================= */
+    @BeforeSuite
+    public void setupReport() {
+        extent = ExtentReportManager.getExtentReport(); // Initialize ExtentReports
+    }
 
     @BeforeClass
     public void setup() {
-
+        // Setup ChromeDriver automatically
         WebDriverManager.chromedriver().setup();
 
-        driver = new ChromeDriver();
+        // Configure ChromeOptions for Jenkins / CI
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");          // Maximize window
+        options.addArguments("--no-sandbox");               // Needed for CI
+        options.addArguments("--disable-dev-shm-usage");    // Overcome limited /dev/shm
+        options.addArguments("--headless=new");             // Headless mode for CI
+        options.addArguments("--disable-gpu");              // Disable GPU (headless)
 
-        driver.manage().window().maximize();
+        // Initialize driver
+        driver = new ChromeDriver(options);
+
+        // Implicit wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+        // Explicit wait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        // Navigate to the application
         driver.get("https://opensource-demo.orangehrmlive.com/");
     }
 
-
-    /* =================================
-       FAILURE SCREENSHOT
-       ================================= */
-
     @AfterMethod
     public void takeScreenshotOnFailure(ITestResult result) {
-
         if (ITestResult.FAILURE == result.getStatus()) {
             AllureUtils.attachScreenshot(driver, "FAILED - " + result.getName());
         }
     }
 
-
-    /* =================================
-       CLOSE ONLY ONCE
-       ================================= */
-
     @AfterClass
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
+        if (extent != null) {
+            extent.flush();
+        }
+    }
+
+    @AfterSuite
+    public void tearDownReport() {
+        if (extent != null) {
+            extent.flush();
+        }
     }
 }
-
-
-
