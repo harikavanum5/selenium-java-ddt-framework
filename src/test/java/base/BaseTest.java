6f06c785050ffdf5271a3308c2f17d83e986c3ff
harikavanum5 <111125660+harikavanum5@users.noreply.github.com>
2026-02-10 17:38:10 +0530

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,6 +2,7 @@
 package base;
 
 import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
